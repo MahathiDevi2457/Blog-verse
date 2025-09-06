ccaebbf5cb861008b2df7c6deefa0e359e67283b
MahathiDevi2457 <mahathidevi2457@gmail.com>
2025-09-06 12:35:57 +0530

    nothing

--- practice.js
+++ practice.js
@@ -1,1 +1,30 @@
 console.log("hello world!,Mahathi Devi")
+
+let name ="Dev Astra";
+console.log(name)
+
+let num=18;
+console.log(num)
+
+let x;
+let y=null;
+
+let islogged=false;
+console.log(y)
+console.log(typeof num)
+console.log(typeof name)
+console.log(typeof islogged)
+console.log(typeof x)
+
+let arr=[1,3,5,7,9];
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"mahathi",
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
+
